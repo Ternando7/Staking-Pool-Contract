ea86256a4571aa3b67708d18675fe35e19c729fa
Alex Kim <lawofambak7@gmail.com>
2022-12-16 18:28:19 -0800

    Remove shares

--- contracts/StakingPool.sol
+++ contracts/StakingPool.sol
@@ -8,41 +8,23 @@ contract StakingPool is Pausable, Ownable {
     // Total amount of ETH staked
     uint256 public totalEthStaked;
 
-    // Total amount of pool shares
-    uint256 public totalPoolShares;
-
     // To check if pool is full or not
     bool public poolFull;
 
     // To check if rewards are turned on
     bool public rewardsOn;
 
-    // Stake info
-    struct StakeInfo {
-        uint256 stakedEth;
-        uint256 poolShares;
-    }
-
-    // Each user's stake info
-    mapping(address => StakeInfo) private userStakeInfo;
+    // User's staked amount
+    mapping(address => uint256) private stakedAmount;
 
     // --- Events ---
-    event Stake(
-        address indexed staker,
-        uint256 stakedAmount,
-        uint256 poolShares
-    );
+    event Stake(address indexed staker, uint256 stakedAmount);
 
-    event WithdrawStake(
-        address indexed staker,
-        uint256 amountWithdrawn,
-        uint256 sharesWithdrawn
-    );
+    event WithdrawStake(address indexed staker, uint256 amountWithdrawn);
 
     event ClaimRewards(
         address indexed staker,
         uint256 amountClaimed,
-        uint256 sharesClaimed,
         uint256 rewardsClaimed
     );
 
@@ -85,142 +67,78 @@ contract StakingPool is Pausable, Ownable {
         // ETH sent from user needs to be greater than 0
         require(msg.value > 0, "Invalid amount");
 
-        // Check if pool is empty
-        if (totalEthStaked == 0 && totalPoolShares == 0) {
-            // User gets 1:1 ratio of pool shares to ETH staked
-            userStakeInfo[msg.sender] = StakeInfo({
-                stakedEth: msg.value,
-                poolShares: msg.value
-            });
+        // Update state
+        stakedAmount[msg.sender] += msg.value;
+        totalEthStaked += msg.value;
 
-            // Update state
-            totalEthStaked += msg.value;
-            totalPoolShares += msg.value;
-
-            emit Stake(msg.sender, msg.value, msg.value);
-        } else {
-            // Calculate user pool shares (includes rewards once they are available)
-            uint256 userShares = (msg.value * totalPoolShares) /
-                address(this).balance;
-
-            // Update user stake info
-            userStakeInfo[msg.sender].stakedEth += msg.value;
-            userStakeInfo[msg.sender].poolShares += userShares;
-
-            // Update pool state
-            totalEthStaked += msg.value;
-            totalPoolShares += userShares;
-
-            emit Stake(msg.sender, msg.value, userShares);
-        }
+        emit Stake(msg.sender, msg.value);
     }
 
     // Allow users to withdraw their stake if pool is not full
     function withdrawStake(uint256 amount) external whenNotPaused isPoolFull {
-        // Get user stake info from storage
-        StakeInfo storage user = userStakeInfo[msg.sender];
-
         // Check if user has enough to withdraw
-        require(user.stakedEth >= amount, "Insufficient amount");
+        require(stakedAmount[msg.sender] >= amount, "Insufficient amount");
 
-        // Calculate user pool shares to withdraw
-        uint256 sharesToWithdraw = (amount * user.poolShares) / user.stakedEth;
-
-        // Update user stake info
-        user.poolShares -= sharesToWithdraw;
-        user.stakedEth -= amount;
-
-        // Update pool state
+        // Update state
+        stakedAmount[msg.sender] -= amount;
         totalEthStaked -= amount;
-        totalPoolShares -= sharesToWithdraw;
 
         // Send user withdrawal amount
         (bool withdrawal, ) = payable(msg.sender).call{value: amount}("");
         require(withdrawal, "Failed to withdraw");
 
-        emit WithdrawStake(msg.sender, amount, sharesToWithdraw);
+        emit WithdrawStake(msg.sender, amount);
     }
 
     // Allow users to unstake a certain amount of ETH + rewards (currently off)
     function unstake(uint256 amount) external whenNotPaused {
         require(rewardsOn, "Currently cannot claim rewards");
 
-        // Get user stake info from storage
-        StakeInfo storage user = userStakeInfo[msg.sender];
+        uint256 userStakedAmount = stakedAmount[msg.sender];
 
         // Check if user has enough to claim
-        require(user.stakedEth >= amount, "Insufficient amount");
+        require(userStakedAmount >= amount, "Insufficient amount");
 
-        // Calculate user pool shares to claim
-        uint256 sharesToClaim = (amount * user.poolShares) / user.stakedEth;
+        // Calculate rewards
+        uint256 totalStakePortion = (userStakedAmount * 10 ** 18) /
+            totalEthStaked;
+        uint256 totalContractRewards = address(this).balance - totalEthStaked;
+        uint256 totalUserRewards = (totalStakePortion * totalContractRewards) /
+            10 ** 18;
+        uint256 rewards = (totalUserRewards *
+            ((amount * 10 ** 18) / userStakedAmount)) / 10 ** 18;
 
-        /**
-         * Calculate Rewards:
-         *
-         * Rewards =
-         *
-         * [(Shares to claim) * (total ETH / total shares)] - [(Shares to claim) * (user staked ETH / user pool shares)]
-         *
-         * @notice Explanation: User shares to claim is multiplied by the total ETH/share ratio to get
-         * how much ETH per share this user should receive. Notice total ETH/share ratio is going to start
-         * from 1 and then continuosly increase as rewards are added to this contract. Then, calculate
-         * how much ETH is staked by the user shares by multiplying user shares to claim by the user
-         * stakedETH/poolShares ratio. After, subtract how much ETH is staked by the user shares from the
-         * total amount of ETH for the user to receive to get how much rewards the user should receive.
-         */
-        uint256 totalReceiveAmount = (sharesToClaim) *
-            ((address(this).balance * (10 ** 18)) / totalPoolShares);
-        uint256 totalStakedAmount = (sharesToClaim) *
-            ((user.stakedEth * (10 ** 18)) / user.poolShares);
-
-        uint256 rewards = (totalReceiveAmount - totalStakedAmount) / 10 ** 18;
-
-        // Update user stake info
-        user.poolShares -= sharesToClaim;
-        user.stakedEth -= amount;
-
-        // Update pool state
+        // Update state
+        stakedAmount[msg.sender] -= amount;
         totalEthStaked -= amount;
-        totalPoolShares -= sharesToClaim;
 
         // Send user amount staked + rewards
         (bool claim, ) = payable(msg.sender).call{value: amount + rewards}("");
         require(claim, "Failed to unstake");
 
-        emit ClaimRewards(msg.sender, amount, sharesToClaim, rewards);
-    }
-
-    // Calculate exchange rate of a share
-    function exchangeRate() public view returns (uint256) {
-        return ((address(this).balance * (10 ** 18)) / totalPoolShares);
+        emit ClaimRewards(msg.sender, amount, rewards);
     }
 
     // Retrieve user's amount of staked ETH
     function stakeOf(address staker) public view returns (uint256) {
-        return userStakeInfo[staker].stakedEth;
+        return stakedAmount[staker];
     }
 
-    // Retrieve user's amount of pool shares
-    function shareOf(address staker) public view returns (uint256) {
-        return userStakeInfo[staker].poolShares;
-    }
-
-    // Calculate a staker's rewards
+    // Calculate total staker's rewards
     function rewardOf(address staker) public view returns (uint256) {
-        uint256 userStake = userStakeInfo[staker].stakedEth;
-        uint256 userShares = userStakeInfo[staker].poolShares;
+        uint256 userStakedAmount = stakedAmount[staker];
 
-        if (userShares == 0) {
+        if (userStakedAmount == 0) {
             return 0;
         }
 
-        uint256 totalReceiveAmount = (userShares) *
-            ((address(this).balance * (10 ** 18)) / totalPoolShares);
-        uint256 totalStakedAmount = (userShares) *
-            ((userStake * (10 ** 18)) / userShares);
+        // Calculate rewards
+        uint256 totalStakePortion = (userStakedAmount * 10 ** 18) /
+            totalEthStaked;
+        uint256 totalContractRewards = address(this).balance - totalEthStaked;
+        uint256 totalUserRewards = (totalStakePortion * totalContractRewards) /
+            10 ** 18;
 
-        uint256 rewards = (totalReceiveAmount - totalStakedAmount) / 10 ** 18;
-
-        return rewards;
+        return totalUserRewards;
     }
 }
